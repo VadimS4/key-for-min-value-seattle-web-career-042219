be879628dae0316cb14716342c43d36f886654f5
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-19 10:40:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, age|
     if age < lowest_value
       lowest_value = age
+      smallest_name = name
       return nil
     else
       return name
